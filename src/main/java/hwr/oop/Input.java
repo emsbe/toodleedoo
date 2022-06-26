ba6231fbf374baf141c0346390548d1ed2fcd4e3
Emma <emma-sophie.betz@web.de>
2022-06-26 11:16:21 +0200

    Created class Input and InputTest to read user input from keyboard
    Co-authored-by: begumnurak <begumnurak@gmail.com>

---
+++ src/main/java/hwr/oop/Input.java
@@ -0,0 +1,32 @@
+package hwr.oop;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+    private PrintStream printStream;
+
+    public Input(InputStream inputStream, PrintStream printStream) {
+        this.scanner = new Scanner(inputStream);
+        this.printStream = printStream;
+    }
+
+    public void enterTaskName() {
+        printStream.println("Enter a task: ");
+        String task = scanner.nextLine();
+        printStream.println("output: " + task);
+    }
+
+    public void enterDate() {
+        printStream.println("Enter a date for the task: ");
+        String date = scanner.nextLine();
+        printStream.println("output: " + date);
+    }
+
+    public void enterDeadline() {
+        printStream.println("Enter a deadline for the task: ");
+        String date = scanner.nextLine();
+        printStream.println("output: " + date);
+    }
+}
---
+++ src/test/java/hwr/oop/InputTest.java
@@ -0,0 +1,59 @@
+package hwr.oop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class InputTest {
+    Input input;
+    private ByteArrayInputStream inputStream;
+    private ByteArrayOutputStream outputStream;
+    private PrintStream printStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        printStream = new PrintStream(outputStream);
+    }
+
+    @Test
+    void input_enterTaskName_returnsTaskName() {
+        inputStream = new ByteArrayInputStream("vacuum\n".getBytes());
+        input = new Input(inputStream, printStream);
+        input.enterTaskName();
+        String outputText = outputStream.toString();
+        String key = "output: ";
+        String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+        assertThat(output).isEqualTo("vacuum");
+    }
+
+    @Test
+    void input_enterDate_returnsDate() {
+        inputStream = new ByteArrayInputStream("23.05.2022\n".getBytes());
+        input = new Input(inputStream, printStream);
+        input.enterDate();
+        String outputText = outputStream.toString();
+        String key = "output: ";
+        String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+        assertThat(output).isEqualTo("23.05.2022");
+    }
+
+    @Test
+    void input_enterDeadline_returnsDeadline() {
+        inputStream = new ByteArrayInputStream("24.05.2022\n".getBytes());
+        input = new Input(inputStream, printStream);
+        input.enterDeadline();
+        String outputText = outputStream.toString();
+        String key = "output: ";
+        String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+        assertThat(output).isEqualTo("24.05.2022");
+    }
+
+
+
+}
