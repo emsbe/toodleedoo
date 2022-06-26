2709eabf655ded7ce31995246ec1b72c29c7ef82
Emma <emma-sophie.betz@web.de>
2022-06-26 14:19:47 +0200

    Added display in three columns.
    Co-authored-by: begumnurak <begumnurak@gmail.com>

--- src/main/java/hwr/oop/KanbanBoard.java
+++ src/main/java/hwr/oop/KanbanBoard.java
@@ -34,4 +34,31 @@ public class KanbanBoard {
     public TaskListOrganizer getDone() {
         return done;
     }
+
+    public void show() {
+        int maxLength = Math.max(Math.max(toDo.getLength(), doing.getLength()), done.getLength());
+        System.out.format("| %-20s | %-20s | %-20s |\n", "To Do", "Doing", "Done");
+        System.out.println("----------------------------------------------------------------------");
+        for (int index = 0; index < maxLength; index++) {
+            outputTasksInColumns(index, maxLength);
+        }
+        System.out.println("----------------------------------------------------------------------");
+    }
+
+    private void outputTasksInColumns(int index, int maxLength) {
+        try {
+            System.out.format("| %-20s | %-20s | %-20s |\n", toDo.getTaskAtIndex(index).getTaskName(), doing.getTaskAtIndex(index).getTaskName(), done.getTaskAtIndex(index).getTaskName());
+        } catch (IndexOutOfBoundsException e) {
+            String forToDo = getStringForOutput(toDo, maxLength, index);
+            String forDoing = getStringForOutput(doing, maxLength, index);
+            String forDone = getStringForOutput(done, maxLength, index);
+            System.out.format("| %-20s | %-20s | %-20s |\n", forToDo, forDoing, forDone);
+        }
+    }
+
+    private String getStringForOutput(TaskListOrganizer taskList, int maxLength, int index) {
+        if (taskList.getLength() < maxLength) {
+            return "";
+        } else return taskList.getTaskAtIndex(index).getTaskName();
+    }
 }
--- src/test/java/hwr/oop/KanbanBoardTest.java
+++ src/test/java/hwr/oop/KanbanBoardTest.java
@@ -59,38 +59,15 @@ public class KanbanBoardTest {
         assertThat(kanbanBoard.getDone().getLength()).isEqualTo(2);
     }
 
-
-
-
-
-
-    /*
-    Interface: KanbanCategory
-    Methods in KanbanCategory: add() - adds task to category, sortByLabel()
-    Three classes that implement KanbanCategory
-    Class KanbanBoard: displays Kanban Categories in three columns
-
-
-    This is very repetitive. In the end, there are multiple array lists that contain the same tasks.
-    Doing it in separate classes requires us to also outsource the sorting algorithm. Because the tasks in each
-    category array list has to be sorted by date as well. Or doesn't it have to be sorted?
-    The problem with having it all in different classes: If a task is deleted or changed in the task list, it also has to be
-    deleted or changed in the kanban-category-list.
-    It seems to make more sense to introduce the kanban-label into the class Task and use the sortBy-methode in taskList
-    to sort by kanban-category-label.
-    The class KanbanBoard is used to display the board.
-
-
-    WICHTIG: Interface TaskOrganizer in TaskList umbenennen, damit das der höchste Datentyp ist. Dann klappt das mit
-    Sorting auch für die Kanban-Kategorien!!!
-
-    ODER:
-    Task bekommt zusätzliches Attribut namens kanbanCategory (vielleicht auch noch direkt das Label mitmachen).
-    Die Tasks sind ja dann in Task List bereits sortiert nach Datum. Wenn man jetzt Listen für jedes Kanban-Label haben möchte,
-    muss man nur einmal durch die TaskList durchgehen und je nach Label der Liste hinzufügen. Aber was macht man dann,
-    wenn ein neuer Task hinzugefügt wird? Ahhhhhhhhhhh.....
-    Observer-Pattern? Haben aber keine richtige 1:n-Abhängigkeit...
-
-     */
+    @Test
+    void kanbanBoard_show_showsThreeColumnsAccordingToKanbanCategories() {
+        toDo.add(taskGym);
+        done.add(taskGym);
+        kanbanBoard.update(toDo, "toDo");
+        kanbanBoard.update(done, "done");
+        kanbanBoard.show();
+        // TODO: Wie kann man das testen?
+    }
+    
 
 }
