a19844e15bf90249e247c078effac55cfb2acc4f
Emma <emma-sophie.betz@web.de>
2022-07-02 17:59:50 +0200

    Added feature to save task lists into files
    Co-authored-by: begumnurak <begumnurak@gmail.com>

---
+++ src/main/java/hwr/oop/FileSaving.java
@@ -0,0 +1,41 @@
+package hwr.oop;
+
+import java.io.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
+public class FileSaving {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public void saveToFile(TaskListOrganizer taskList, String listType) throws IOException {
+        try {
+            File file = new File("src/test/java/hwr/oop/resources/" + listType + ".txt");
+            PrintWriter pw = new PrintWriter(file);
+            for (int index = 0; index < taskList.getLength(); index++) {
+                if (index < taskList.getLength()-1) {
+                    pw.write(taskToString(taskList.getTaskAtIndex(index)) + String.format("%n"));
+                } else pw.write(taskToString(taskList.getTaskAtIndex(index)));
+            }
+            pw.flush();
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("Your task list "+listType+" could not be saved. Try again.");
+        }
+        System.out.println(listType+" has been successfully saved.");
+        /*
+        try (ObjectOutputStream out = new ObjectOutputStream((new FileOutputStream("src/test/java/hwr/oop/resources/"+listType+".txt")))) {
+            for (int index = 0; index < taskList.getLength(); index++) {
+                out.writeObject(taskToString(taskList.getTaskAtIndex(index))+String.format("%n"));
+            }
+            System.out.println("Save successful.");
+        } catch (IOException e) {
+            System.out.println("File to write the Task list in couldn't be found. Try again.");
+        }
+
+         */
+    }
+
+    private String taskToString(Task task) {
+        return String.format("%s;%s;%s", task.getTaskName(), task.getDate().toString(), task.getDeadline().toString());
+    }
+}
