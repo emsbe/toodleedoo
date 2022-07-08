1dd62b84da62984019a93aa2bc482be4cb32dcab
Emma <emma-sophie.betz@web.de>
2022-07-08 09:45:41 +0200

    cleanup

--- src/main/java/hwr/oop/toodleedoo/FileSaving.java
+++ src/main/java/hwr/oop/toodleedoo/FileSaving.java
@@ -6,27 +6,6 @@ import java.util.Objects;
 
 public class FileSaving {
 
-    /*public void saveToFile(TaskList taskList, String listType) {
-        if (Objects.equals(listType, "taskList") || Objects.equals(listType, "toDo") || Objects.equals(listType, "doing") || Objects.equals(listType, "done")) {
-            try {
-                File file = new File("src/test/java/hwr/oop/toodleedoo/resources/" + listType + ".txt");
-                PrintWriter pw = new PrintWriter(file);
-                for (int index = 0; index < taskList.getLength(); index++) {
-                    if (index < taskList.getLength()-1) {
-                        pw.write(taskToString(taskList.getTaskAtIndex(index)) + String.format("%n"));
-                    } else pw.write(taskToString(taskList.getTaskAtIndex(index)));
-                }
-                pw.flush();
-                pw.close();
-            } catch (IOException e) {
-                System.out.println("Your task list "+listType+" could not be saved. Try again.");
-            }
-            System.out.println(listType+" has been successfully saved.");
-        } else {
-            System.out.println("Invalid list type.");
-        }
-    }*/
-
     public void saveToFile(TaskList taskList, String listType) throws FileNotFoundException {
         if (Objects.equals(listType, "taskList") || Objects.equals(listType, "toDo") || Objects.equals(listType, "doing") || Objects.equals(listType, "done")) {
             File file = new File("src/test/java/hwr/oop/toodleedoo/resources/" + listType + ".txt");
